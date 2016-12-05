4b4477401d251a2413478363b624e2d7cd528ce1
htchepannou <htchepannou@expedia.com>
2016-12-05 04:06:10 -0500

    output SHA

--- src/main/java/io/tchepannou/kribi/config/AwsConfiguration.java
+++ src/main/java/io/tchepannou/kribi/config/AwsConfiguration.java
@@ -2,13 +2,18 @@ package io.tchepannou.kribi.config;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.PropertiesFileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class AwsConfiguration {
+    @Autowired
+    Environment env;
 
     //-- Beans
     @Bean
@@ -18,7 +23,13 @@ public class AwsConfiguration {
     }
 
     @Bean
-    AWSCredentialsProvider awsCredentialsProvider(){
-        return new DefaultAWSCredentialsProviderChain();
+    AWSCredentialsProvider awsCredentialsProvider() {
+        if (env.acceptsProfiles("dev")){
+            final String home = System.getProperty("user.home");
+            return new PropertiesFileCredentialsProvider(home + "/.aws/credentials");
+        } else {
+            return new DefaultAWSCredentialsProviderChain();
+        }
     }
+
 }
