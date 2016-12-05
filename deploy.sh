441565f565fe1c2957dc96313e03f0cd53916db0
htchepannou <htchepannou@expedia.com>
2016-12-05 04:15:19 -0500

    use TRAVIS_COMMIT variable

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
-echo "TRAVIS_PULL_REQUEST_SHA=$TRAVIS_PULL_REQUEST_SHA"
+echo "TRAVIS_COMMIT=$TRAVIS_COMMIT"
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-  curl -X POST -d @target/kribi.jar -H "api_key: $KRIBI_API_KEY" http://kribi.tchepannou.io/artifact/kribi/$TRAVIS_PULL_REQUEST_SHA
+  curl -X POST -d @target/kribi.jar -H "api_key: $KRIBI_API_KEY" http://kribi.tchepannou.io/artifact/kribi/$TRAVIS_COMMIT
 fi
