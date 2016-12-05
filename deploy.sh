37030e0da4ad734f9cd064f722887080efb19634
htchepannou <htchepannou@expedia.com>
2016-12-05 04:29:31 -0500

    fix endpoints

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
 echo "TRAVIS_COMMIT=$TRAVIS_COMMIT"
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-  curl -X POST -d @target/kribi.jar -H "api_key: $KRIBI_API_KEY" http://kribi.tchepannou.io/artifact/kribi/$TRAVIS_COMMIT
+  curl -X POST -d @target/kribi.jar -H "api_key: $KRIBI_API_KEY" http://kribi.tchepannou.io/v1/application/artifact/kribi/$TRAVIS_COMMIT
 fi
