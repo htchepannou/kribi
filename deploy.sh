0eba307b87ca9b1109271f136b2c26c191be2d61
htchepannou <htchepannou@expedia.com>
2016-12-05 03:48:21 -0500

    fix delpoy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   curl -X POST -d @target/kribi.jar -H "api_key: $KRIBI_API_KEY" http://kribi.tchepannou.io/artifact/kribi/$TRAVIS_PULL_REQUEST_SHA
 fi
