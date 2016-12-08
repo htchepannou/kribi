613fe48b651b16c14f00a31cf1734de1b46cd694
htchepannou <htchepannou@expedia.com>
2016-12-08 18:38:34 -0500

    fix deployment

--- deploy_after.sh
+++ deploy_after.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-  wget http://kribi.tchepannou.io/v1/application/kribi/init_artifact?version=$TRAVIS_COMMIT
+  curl -H "api_key: $KRIBI_API_KEY" http://kribi.tchepannou.io/v1/application/kribi/init_artifact?version=$TRAVIS_COMMIT
 
-  wget http://kribi.tchepannou.io/v1/application/kribi/deploy?name=kribi&environment=PROD&region=us-east-1&undeployOld=true&version=$TRAVIS_COMMIT
+  curl -H "api_key: $KRIBI_API_KEY" http://kribi.tchepannou.io/v1/application/kribi/deploy?name=kribi&environment=PROD&region=us-east-1&undeployOld=true&version=$TRAVIS_COMMIT
   exit $?
 
 fi
