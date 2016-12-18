9e2220e077eeb30b0100b3176046b6643fe1afaf
htchepannou <htchepannou@expedia.com>
2016-12-17 22:03:14 -0500

    set timeout

--- deploy_after.sh
+++ deploy_after.sh
@@ -2,13 +2,12 @@
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   ARTIFACT_URL="http://kribi.tchepannou.io/v1/application/kribi/init_artifact?version=$TRAVIS_COMMIT"
-  echo "curl $ARTIFACT_URL"
   curl -v -H "api_key: $KRIBI_API_KEY" $ARTIFACT_URL
 
   echo
+  echo
   DEPLOY_URL="http://kribi.tchepannou.io/v1/application/kribi/deploy?environment=PROD&region=us-east-1&release=true&undeployOld=true&version=$TRAVIS_COMMIT"
-  echo "curl $DEPLOY_URL"
-  curl -v -H "api_key: $KRIBI_API_KEY" $DEPLOY_URL
+  curl -v -H "api_key: $KRIBI_API_KEY" -m 600 $DEPLOY_URL
   exit $?
 
 fi
